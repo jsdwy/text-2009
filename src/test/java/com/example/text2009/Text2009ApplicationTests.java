e3c568a48a77c18154331aee7d17809646c0e3d5
xin <1111>
2021-08-31 19:38:41 +0800

    项目初始化

---
+++ src/test/java/com/example/text2009/Text2009ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.text2009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Text2009ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
